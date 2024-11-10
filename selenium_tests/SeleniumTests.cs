110563d327b70d6aea3ecae9075dd35796e61dcc
imdebamrita <imdebamrita@gmail.com>
2024-11-10 19:49:26 +0530

    initial commit

---
+++ selenium_tests/SeleniumTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace selenium_tests;
+
+public class SeleniumTests
+{
+    [Test]
+    [Category("Selenium")]
+
+    public void FirstTest()
+    {
+        IWebDriver driver = new ChromeDriver();
+        driver.Navigate().GoToUrl("https://www.selenium.dev");
+        Assert.That(driver.Title, Is.EqualTo("Selenium"));
+        driver.Quit();
+    }
+}
